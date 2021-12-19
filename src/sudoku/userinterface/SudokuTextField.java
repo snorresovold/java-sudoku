3c1f86ea7dde5787a130b3f92add014e5e3b36f2
snorresovold <73354016+snorresovold@users.noreply.github.com>
2021-12-19 01:44:10 +0100

    updates

--- src/sudoku/userinterface/SudokuTextField.java
+++ src/sudoku/userinterface/SudokuTextField.java
@@ -22,15 +22,15 @@ public class SudokuTextField extends  TextField {
 
     @Override
     public void replaceText(int i, int i1, String s){
-        if (!s.matches(regex:"[0-9]")) {
+        if (!s.matches("[0-9]")) {
             super.replaceText(i, i1, s);
         }
     }
 
     @Override
     public void replaceSelection(String s) {
-        if (!s.matches(regex:"[0-9]")) {
-            super.replaceText(s);
+        if (!s.matches("[0-9]")) {
+            super.replaceSelection(s);
         }
     }
 }
--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -28,15 +28,52 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
 
     private static final  Color WINDOW_BACKGROUND_COLOR = Color.rgb(0, 150, 136);
     private static final  Color BOARD_BACKGROUND_COLOR = Color.rgb(224, 242, 241);
+    private static final String SUDOKU = "Sudoku";
 
-    @Override
-    public void handle(KeyEvent keyEvent) {
+    public UserInterfaceImpl(Stage stage) {
+        this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterFace();
+    }
 
+    private void initializeUserInterFace() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextField(root);
+        drawGridLines(root);
+        stage.show();
+    }
+
+    private void drawGridLines(Group root) {
+        int xAndY = 114;
+        int index = 0;
+        while (index < 8) {
+            int thickness;
+            if (index == 2 || 5) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+        }
+    }
+
+    private void drawTextField(Group root) {
+    }
+
+    private void drawSudokuBoard(Group root) {
+    }
+
+    private void drawTitle(Group root) {
+    }
+
+    private void drawBackground(Group root) {
     }
 
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
-
+        this.listener = listener
     }
 
     @Override
@@ -56,6 +93,11 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
 
     @Override
     public void showError(String Message) {
+
+    }
+
+    @Override
+    public void handle(keyEvent keyEvent) {
 
     }
 }
